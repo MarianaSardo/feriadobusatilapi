2641af4924fa8291c6484fa158ea6f2e2367e95f
MarianaSardo <linkerita@gmail.com>
2025-02-09 15:47:35 -0300

    up

--- main.py
+++ main.py
@@ -108,7 +108,3 @@ def eliminar_feriado(anio: int, fecha: str):
 
     return {"mensaje": f"Feriado del {fecha} eliminado correctamente."}
 
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
